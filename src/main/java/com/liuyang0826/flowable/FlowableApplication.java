096295b2a0b55e55fdec22e297a32ab865416d45
liuyang.0826 <1161906592@qq.com>
2024-02-06 19:33:25 +0800

    initial commit

---
+++ src/main/java/com/liuyang0826/flowable/FlowableApplication.java
@@ -0,0 +1,13 @@
+package com.liuyang0826.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableApplication.class, args);
+	}
+
+}
